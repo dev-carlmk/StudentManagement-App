fa25e5a9815d5e74d3aad6f1ad01b3d21cd69f29
T. Carlos Kerwillain <tcarloskerwillain@gmail.com>
2026-05-08 02:08:28 +0200

    Add MySQL database connection setup using JDBC driver

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,18 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class DatabaseConnection{
-    public static Connection getConnection(){
-        try{
+public class DatabaseConnection {
+
+    public static Connection getConnection() {
+        try {
             String url = "jdbc:mysql://localhost:3306/student_db";
             String user = "root";
             String password = "";
 
-            Connection conn = DriverManager.getConnection(url, user, password);
-            return conn;
-        
-        }catch(Exception e){
-            System.out.println(e);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection(url, user, password);
+
+        } catch (Exception e) {
+            System.out.println("DB Connection Error: " + e.getMessage());
             return null;
         }
     }
